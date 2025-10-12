fe49dca591e859a26a76281a7f7416f147aa4dc5
vikash-sharma-4991 <vikash-sharma-4991@vikashsharma4991.in>
2025-10-12 23:02:43 +0530

    Home Page Design

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,74 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { dummyProducts } from "../assets/assets";
+import toast from "react-hot-toast";
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const AppContext = createContext();
+
+export const AppContextProvider = ({children}) => {
+
+    const currency = import.meta.VITE_CURRENCY;
+
+    const navigate = useNavigate();
+    const [user, setUser] = useState(null);   
+    const [isSeller, setIsSeller] = useState(false);
+    const [showUserLogin, setShowUserLogin] = useState(false);
+    const [products, setProducts] = useState([]);
+
+    const [cartItems, setCartItems] = useState({})
+
+    //fetch all products
+    const fetchProducts = async() => {
+        setProducts(dummyProducts);
+    }
+
+    //add product to cart
+    const addToCart = (itemId)=> {
+        let cartData = structuredClone(cartItems);
+
+        if(cartData[itemId]){
+            cartData[itemId] += 1
+        }else{
+            cartData[itemId] = 1
+        }
+        setCartItems(cartData);
+        toast.success("Added to Cart")
+    }
+
+    //update cart item quantity
+    const updateCartItem = (itemId, quantity)=> {
+        let cartData = structuredClone(cartItems);
+        cartData[itemId] = quantity;
+        setCartItems(cartData);
+        toast.success("Cart Updated")
+    }
+
+    //Remove product from cart
+    const removeFromCart = (itemId) => {
+        let cartData = structuredClone(cartItems);
+        if(cartData[itemId]){
+            cartData[itemId] -= 1;
+            if(cartData[itemId] === 0){
+                delete cartData[itemId]
+            }
+        }
+        toast.success("Remove From Cart");
+        setCartItems(cartData)
+    }
+
+    useEffect(()=> {
+        fetchProducts();
+    },[])
+
+    const value = {navigate, user , setUser, setIsSeller, isSeller, showUserLogin, setShowUserLogin, products, currency, addToCart, updateCartItem, removeFromCart, cartItems};
+    
+    return <AppContext.Provider value={value}>
+        {children}
+        </AppContext.Provider>
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useAppContext = () => {
+    return useContext(AppContext)
+}
