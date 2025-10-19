6e48d13c9cf4fef979314ccea832b84c5168306b
vikash-sharma-4991 <vikash-sharma-4991@vikashsharma4991.in>
2025-10-19 21:32:38 +0530

    Categories Page Create

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Footer from './components/footer';
 import { useAppContext } from './context/AppContext';
 import Login from './components/Login';
 import AllProducts from './pages/AllProducts';
+import ProductCategory from './pages/ProductCategory';
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller")
@@ -24,6 +25,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/products' element={<AllProducts/>}/>
+        <Route path='/products/:category' element={<ProductCategory/>}/>
       </Routes>
       </div>
       {!isSellerPath && <Footer/>}
